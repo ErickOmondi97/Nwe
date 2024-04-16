4f9bfc75ea0020592d7924fabd5132e9ddee8aa6
Erick Omondi <eriqueasaria@gmail.com>
2024-04-16 22:53:45 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/screens/*.{js,jsx,ts,tsx}", "./components/*"],
   theme: {
     extend: {},
   },
