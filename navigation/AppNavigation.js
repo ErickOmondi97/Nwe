5bf9f659b53aef23e9c09298262efa368dbffa6c
Erick Omondi <eriqueasaria@gmail.com>
2024-04-19 11:43:03 +0300

    Add files via upload

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from '../src/screens/HomeScreen';
-import App from "../App";
+import MovieScreen from "../src/screens/movieScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +10,7 @@ export default function AppNavigation() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen}/>
+                <Stack.Screen name="Movie" options={{headerShown: false}} component={MovieScreen}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
